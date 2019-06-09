163d2959ece0f6c185a238a0d9f04b44fead9ca1
engAnt <codersellu@outlook.com>
2019-06-09 11:48:55 -0500

    quick update to example

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ in one way or another. For example, [Blueprints](http://flask.pocoo.org/docs/0.1
 is the way to go if you decide to use Flask.
 
 
+For this example the Gapminder dataset is used. With just one Shiny module, we
+are able to explore the data by continents.
+By scoping the repeated logic within a module, code duplication is avoived
+since the data used on each tab differs by the filtering argument used.
+
 ## Setup
  - Install RStudio
  - Install _shiny_ package
--- app.R
+++ app.R
@@ -1,7 +1,18 @@
 library(shiny)
 library(dplyr)
-source(file.path("modules", "data.R"))
+
 source(file.path("modules", "gapModule.R"))
+library(gapminder)
+
+# Note: This code creates data sets to use in each tab.
+# It removes Kuwait since Kuwait distorts the gdp scale
+all_data <- filter(gapminder, country != "Kuwait")
+africa_data <- filter(gapminder, continent == "Africa")
+americas_data <- filter(gapminder, continent == "Americas")
+asia_data <- filter(gapminder, continent == "Asia", country != "Kuwait")
+europe_data <- filter(gapminder, continent == "Europe")
+oceania_data <- filter(gapminder, continent == "Oceania")
+
 
 ui <- fluidPage(
   tags$style(type="text/css", ".recalculating { opacity: 1.0; }"),
